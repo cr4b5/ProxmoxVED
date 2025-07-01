69503d312e966580c47efeb9f26be6b273066e1e
cr4b5 <kw@kilocore.xyz>
2025-06-30 18:34:19 -0700

    initial commit for raw url changes and file additionas

---
+++ install/pangolin-install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Cr4b5
+# License: AGPL-3.0 | https://github.com/fosrl/pangolin?tab=AGPL-3.0-1-ov-file#readme
+# Source: https://github.com/fosrl/pangolin
+
+# Import Functions und Setup
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+get_latest_release() {
+    curl -fsSL https://api.github.com/repos/"$1"/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+
+DOCKER_LATEST_VERSION=$(get_latest_release "moby/moby")
+PANGOLIN_LATEST_VERSION=$(get_latest_release "fosrl/pangolin")
+#PORTAINER_AGENT_LATEST_VERSION=$(get_latest_release "portainer/agent")
+#DOCKER_COMPOSE_LATEST_VERSION=$(get_latest_release "docker/compose")
+
+msg_info "Installing Docker $DOCKER_LATEST_VERSION"
+DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
+mkdir -p $(dirname $DOCKER_CONFIG_PATH)
+echo -e '{\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
+$STD sh <(curl -fsSL https://get.docker.com)
+msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
+
+msg_info "Installing Pangolin $PANGOLIN_LATEST_VERSION"
+#mkdir -p pangolin
+$STD sh <(wget -O installer "https://github.com/fosrl/pangolin/releases/download/1.6.1/installer_linux_$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')" && chmod +x ./installer)
+msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
