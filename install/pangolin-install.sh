5d570ef4d481d7e1739b35aba98bf82c96ea54d5
cr4b5 <kw@kilocore.xyz>
2025-06-30 20:14:23 -0700

    removed portainer items from pangolin

--- install/pangolin-install.sh
+++ install/pangolin-install.sh
@@ -29,34 +29,6 @@ echo -e '{\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
 $STD sh <(curl -fsSL https://get.docker.com)
 msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
 
-read -r -p "${TAB3}Would you like to add Portainer? <y/N> " prompt
-if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
-    msg_info "Installing Portainer $PORTAINER_LATEST_VERSION"
-    docker volume create portainer_data >/dev/null
-    $STD docker run -d \
-        -p 8000:8000 \
-        -p 9443:9443 \
-        --name=portainer \
-        --restart=always \
-        -v /var/run/docker.sock:/var/run/docker.sock \
-        -v portainer_data:/data \
-        portainer/portainer-ce:latest
-    msg_ok "Installed Portainer $PORTAINER_LATEST_VERSION"
-else
-    read -r -p "${TAB3}Would you like to add the Portainer Agent? <y/N> " prompt
-    if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
-        msg_info "Installing Portainer agent $PORTAINER_AGENT_LATEST_VERSION"
-        $STD docker run -d \
-            -p 9001:9001 \
-            --name portainer_agent \
-            --restart=always \
-            -v /var/run/docker.sock:/var/run/docker.sock \
-            -v /var/lib/docker/volumes:/var/lib/docker/volumes \
-            portainer/agent
-        msg_ok "Installed Portainer Agent $PORTAINER_AGENT_LATEST_VERSION"
-    fi
-fi
-
 motd_ssh
 customize
 
